f5d79d5278b4c85e6ab6e498f7b0f79ddd025f59
Jorge Castro <david92_jc@yahoo.es>
2023-02-06 17:19:51 -0500

    Cambios a los estilos.

--- src/components/NavbarMain.jsx
+++ src/components/NavbarMain.jsx
@@ -54,9 +54,9 @@ const NavbarMain = () => {
                         </a>
                         <NavLink to="/login" activeclassname="active" className="nav-icon position-relative text-decoration-none">
                             <i className="fa fa-fw fa-user text-dark mr-3"></i>
-                            <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">
+{/*                             <span className="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">
                                 +99
-                            </span>
+                            </span> */}
                         </NavLink>
                     </div>
                 </div>
