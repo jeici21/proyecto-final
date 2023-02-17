dbc68455b1e6780deb4b701b3c0d22d303481988
Piguave <ariel_97_xd@hotmail.com>
2023-02-17 12:48:03 -0500

    detalles de estilo v 0.3

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,93 +1,75 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 const FeaturedProducts = () => {
+    const [products, setProducts] = useState([]);
+    const ProductUrl = "http://localhost:8080/product";
+    // Obtener datos de productos
+    const peticionGetProducts = async () => {
+        await axios.get(ProductUrl)
+            .then(response => {
+                setProducts(response.data);;
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+    const filteredProducts = products
+    ? products
+        .filter((product) => product)
+        .sort((a, b) => b.dimensions - a.dimensions)
+        .slice(0, 3)
+    : products;
+    useEffect(() => {
+        peticionGetProducts();
+    }, []);
     return (
         <section className="bg-light">
             <div className="container py-5">
                 <div className="row text-center py-3">
                     <div className="col-lg-6 m-auto">
                         <h1 className="h1">Productos destacados</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                        </p>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="floating col-12 col-md-4 mb-4">
-                        <div className="card h-100">
-                            <a href="#?">
-                                <img src="https://therichpost.com/wp-content/uploads/2021/05/feature_prod_01.jpg"
-                                    className="card-img-top" alt="..." />
-                            </a>
-                            <div className="card-body">
-                                <ul className="list-unstyled d-flex justify-content-between">
-                                    <li>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-muted fa fa-star"></i>
-                                        <i className="text-muted fa fa-star"></i>
-                                    </li>
-                                    <li className="text-muted text-right">$240.00</li>
-                                </ul>
-                                <a href="#?" className="h2 text-decoration-none text-dark">Lorem Ipsum</a>
-                                <p className="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                                </p>
-                                <p className="text-muted">Reviews (24)</p>
+                    {filteredProducts.map((result) => {
+                        return (
+                            <div className="floating col-12 col-md-4 mb-4 " key={result.id}>
+                                <div className="card h-100">
+                                    <div className="row">
+                                        <div className="col-12 d-flex justify-content-center">
+                                            <NavLink
+                                                to={`/details/id:${result.id}`}
+                                                title="Vistazo Rápido"
+                                                className="bg"
+                                            >
+                                                <img src={result.img} className="" width="300px" />
+                                            </NavLink>
+                                        </div>
+                                    </div>
+                                    <div className="card-body">
+                                        <ul className="list-unstyled d-flex justify-content-between">
+                                            <li>
+                                                <i className="text-warning fa fa-star"></i>
+                                                <i className="text-warning fa fa-star"></i>
+                                                <i className="text-warning fa fa-star"></i>
+                                                <i className="text-warning fa fa-star"></i>
+                                                <i className="text-muted fa fa-star"></i>
+                                            </li>
+                                            <li className="text-muted text-right">${result.price}</li>
+                                        </ul>
+                                        <a href="#?" className="h2 text-decoration-none text-dark">{result.name}</a>
+                                        <div className="flex-wrap">
+                                            <p className="card-text text-truncate">
+                                                {result.longdesc}
+                                            </p>
+                                        </div>
+                                        <p className="text-muted">Reviews (24)</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="floating col-12 col-md-4 mb-4">
-                        <div className="card h-100">
-                            <a href="#?">
-                                <img src="https://therichpost.com/wp-content/uploads/2021/05/feature_prod_02.jpg"
-                                    className="card-img-top" alt="..." />
-                            </a>
-                            <div className="card-body">
-                                <ul className="list-unstyled d-flex justify-content-between">
-                                    <li>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-muted fa fa-star"></i>
-                                        <i className="text-muted fa fa-star"></i>
-                                    </li>
-                                    <li className="text-muted text-right">$480.00</li>
-                                </ul>
-                                <a href="#?" className="h2 text-decoration-none text-dark">Lorem Ipsum</a>
-                                <p className="card-text">
-                                    Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum
-                                </p>
-                                <p className="text-muted">Reviews (48)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="floating col-12 col-md-4 mb-4">
-                        <div className="card h-100">
-                            <a href="#?">
-                                <img src="https://therichpost.com/wp-content/uploads/2021/05/feature_prod_03.jpg"
-                                    className="card-img-top" alt="..." />
-                            </a>
-                            <div className="card-body">
-                                <ul className="list-unstyled d-flex justify-content-between">
-                                    <li>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                        <i className="text-warning fa fa-star"></i>
-                                    </li>
-                                    <li className="text-muted text-right">$360.00</li>
-                                </ul>
-                                <a href="#?" className="h2 text-decoration-none text-dark">Lorem Ipsum</a>
-                                <p className="card-text">
-                                    Lorem Ipsum Lorem Ipsum Lorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum.
-                                </p>
-                                <p className="text-muted">Reviews (74)</p>
-                            </div>
-                        </div>
-                    </div>
+                        );
+                    })}
+
                 </div>
             </div>
         </section>
